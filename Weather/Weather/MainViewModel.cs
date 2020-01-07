6e0eca452075f1b8197d6562d57551c5de6cabf4
uberthought@outlook.com <uberthought@outlook.com>
2020-01-07 12:11:50 -0500

    initial version

---
+++ Weather/Weather/MainViewModel.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Text;
+using System.Windows.Input;
+using Xamarin.Forms;
+
+namespace Weather
+{
+    public class MainViewModel : INotifyPropertyChanged
+    {
+        public MainViewModel()
+        {
+            Refresh = new Command(
+                execute: () =>
+                {
+                    updated = "Hello, World!";
+                    PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(nameof(Updated)));
+                });
+        }
+        public event PropertyChangedEventHandler PropertyChanged;
+
+        string updated = String.Empty;
+
+        public String Updated
+        {
+            get { return updated; }
+            set
+            {
+                PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(updated));
+            }
+        }
+        public ICommand Refresh { private set; get; }
+
+    }
+}
