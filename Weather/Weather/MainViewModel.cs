5ccf02dce45b2fcb4a0ffeda99be2c43349b968a
uberthought@outlook.com <uberthought@outlook.com>
2020-01-08 13:02:28 -0500

    Added location label binding fake a network request to test that UI updates after request returns started adding location (lat, lon) os request

--- Weather/Weather/MainViewModel.cs
+++ Weather/Weather/MainViewModel.cs
@@ -4,32 +4,55 @@ using System.ComponentModel;
 using System.Text;
 using System.Windows.Input;
 using Xamarin.Forms;
+using Xamarin.Essentials;
+using System.Threading.Tasks;
 
 namespace Weather
 {
     public class MainViewModel : INotifyPropertyChanged
     {
-        public MainViewModel()
-        {
-            Refresh = new Command(
-                execute: () =>
-                {
-                    updated = "Hello, World!";
-                    PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(nameof(Updated)));
-                });
-        }
         public event PropertyChangedEventHandler PropertyChanged;
 
-        string updated = String.Empty;
+        public MainViewModel()
+        {
+            // setup Refresh button binding
+            Refresh = new Command(
+                execute: async () =>
+                {
+                    // show fake data
+                    updated = DateTime.Now.ToString("dd MMM hh:mm tt");
+                    location = "Fake Location";
+                    PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(nameof(Updated)));
+                    PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(nameof(Location)));
 
+                    // delay to simulate a network request
+                    await Task.Delay(1000);
+
+                    // show updated fake data after fake network request
+                    updated = DateTime.Now.ToString("dd MMM hh:mm tt");
+                    location = "Updated Fake Location";
+                    PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(nameof(Updated)));
+                    PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(nameof(Location)));
+                });
+        }
+
+        // Updated label binding
+        string updated = String.Empty;
         public String Updated
         {
             get { return updated; }
-            set
-            {
-                PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(updated));
-            }
+            set { PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(updated)); }
         }
+
+        // Location label binding
+        string location = String.Empty;
+        public String Location
+        {
+            get { return location; }
+            set { PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(location)); }
+        }
+
+        // Refresh button binding
         public ICommand Refresh { private set; get; }
 
     }
