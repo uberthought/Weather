9f5bd2a905f4f9a0f273f82561f8cedb62ce5ba9
uberthought@outlook.com <uberthought@outlook.com>
2020-01-13 17:28:45 -0500

    Add device orientation support

--- Weather/Weather.Android/MainActivity.cs
+++ Weather/Weather.Android/MainActivity.cs
@@ -9,7 +9,7 @@ using Android.OS;
 
 namespace Weather.Droid
 {
-    [Activity(Label = "Weather", Icon = "@mipmap/icon", Theme = "@style/MainTheme", MainLauncher = true, ConfigurationChanges = ConfigChanges.ScreenSize | ConfigChanges.Orientation)]
+    [Activity(Label = "Weather", Icon = "@mipmap/icon", Theme = "@style/MainTheme", MainLauncher = true, ConfigurationChanges = ConfigChanges.ScreenSize | ConfigChanges.Orientation, ScreenOrientation = ScreenOrientation.FullUser)]
     public class MainActivity : global::Xamarin.Forms.Platform.Android.FormsAppCompatActivity
     {
         protected override void OnCreate(Bundle savedInstanceState)
